8a064b30dd89929006362460a1bbbc9e0a062f23
W4LX <crocraxkerw@gmail.com>
2024-09-26 04:04:09 -0500

    Changes

--- createSticker.js
+++ createSticker.js
@@ -1,109 +1,105 @@
 const { readFile, unlink } = require("fs/promises");
-const { Stream, Readable } = require("stream");
+const { Readable } = require("stream");
 const { spawn } = require("child_process");
 const { tmpdir } = require("os");
 const { resolve } = require("path");
-const embedMetadata = require("./metadata");
 const sharp = require("sharp");
+const embedMetadata = require("./metadata");
 
-async function createSticker(input, options = {}) {
+async function createSticker(input, { ffmpeg, metadata } = {}) {
   if (!input) {
     throw new Error("An input file was not provided.");
   }
 
-  if (input instanceof Stream) {
-    input = await streamToBuffer(input);
+  if (!Buffer.isBuffer(input) && !(input instanceof Readable)) {
+    throw new Error("You must pass a stream or buffer");
   }
 
-  if (!Buffer.isBuffer(input)) {
+  const buffer =
+    input instanceof Readable ? await streamToBuffer(input) : input;
+
+  if (!Buffer.isBuffer(buffer)) {
     throw new Error("The input file is not a valid buffer.");
   }
 
   const { fileTypeFromBuffer } = await import("file-type");
+  const type = await fileTypeFromBuffer(buffer);
 
-  const type = await fileTypeFromBuffer(input);
-
-  if (type.mime.includes("image")) {
-    const sticker = await sharp(input)
-      .webp()
-      .resize(512, 512, {
-        fit: "contain",
-        background: { r: 0, g: 0, b: 0, alpha: 0 },
-      })
-      .toBuffer();
-
-    return await embedMetadata(sticker, options.metadata);
-  } else if (type.mime.includes("video")) {
-    if (!options.ffmpeg) {
-      throw new Error("The ffmpeg path is not specified.");
-    }
-
-    const stream = new Readable();
-    stream.push(input);
-    stream.push(null);
-
-    const output = resolve(tmpdir(), `${Date.now()}.webp`);
-
-    try {
-      const args = [
-        "-i",
-        "-",
-        "-vcodec",
-        "libwebp",
-        "-vf",
-        "scale='iw*min(300/iw,300/ih)':'ih*min(300/iw,300/ih)',format=rgba,pad=300:300:'(300-iw)/2':'(300-ih)/2':'#00000000',setsar=1,fps=10",
-        "-loop",
-        "0",
-        "-ss",
-        "00:00:00.0",
-        "-t",
-        "00:00:05.0",
-        "-an",
-        "-vsync",
-        "0",
-        "-s",
-        "512:512",
-        "-qscale:v",
-        "50",
-        output,
-      ];
-
-      await new Promise((resolve, reject) => {
-        const ffmpeg = spawn(options.ffmpeg, args);
-        stream.pipe(ffmpeg.stdin);
-
-        ffmpeg.stdin.on("error", () => null);
-
-        ffmpeg.on("exit", (code) => {
-          if (code === 0) {
-            resolve(true);
-          } else {
-            reject(`FFmpeg exited with code ${code}`);
-          }
-        });
-      });
-
-      const sticker = await readFile(output);
-
-      return await embedMetadata(sticker, options.metadata);
-    } catch (error) {
-      throw error;
-    } finally {
-      unlink(output).catch(() => null);
-    }
+  if (type?.mime?.startsWith("image")) {
+    return await processImage(buffer, metadata);
+  } else if (type?.mime?.startsWith("video")) {
+    if (!ffmpeg) throw new Error("The ffmpeg path is not specified.");
+    return await processVideo(buffer, ffmpeg, metadata);
   } else {
     throw new Error("The file is neither a valid image nor a video.");
   }
 }
 
-function streamToBuffer(stream) {
-  return new Promise((resolve, reject) => {
-    const chunks = [];
+async function processImage(buffer, metadata) {
+  const options = {
+    fit: "contain",
+    background: { r: 0, g: 0, b: 0, alpha: 0 },
+  };
 
-    stream.on("data", (chunk) => chunks.push(chunk));
-    stream.on("end", () => resolve(Buffer.concat(chunks)));
-    stream.on("error", (error) => reject(error));
-  });
+  const sticker = await sharp(buffer)
+    .webp()
+    .resize(512, 512, options)
+    .toBuffer();
+
+  return await embedMetadata(sticker, metadata);
+}
+
+async function processVideo(buffer, ffmpeg, metadata) {
+  const output = resolve(tmpdir(), `${Date.now()}.webp`);
+
+  const args = [
+    "-i",
+    "-",
+    "-vcodec",
+    "libwebp",
+    "-vf",
+    "scale='iw*min(300/iw,300/ih)':'ih*min(300/iw,300/ih)',format=rgba,pad=300:300:'(300-iw)/2':'(300-ih)/2':'#00000000',setsar=1,fps=10",
+    "-loop",
+    "0",
+    "-t",
+    "5",
+    "-an",
+    "-vsync",
+    "0",
+    "-s",
+    "512:512",
+    "-qscale:v",
+    "50",
+    output,
+  ];
+
+  try {
+    await new Promise((resolve, reject) => {
+      const ffmpegProc = spawn(ffmpeg, args);
+
+      ffmpegProc.stdin.write(buffer);
+      ffmpegProc.stdin.end();
+      ffmpegProc.stdin.on("error", () => null);
+      ffmpegProc.on("close", (code) => {
+        if (code === 0) {
+          resolve(true);
+        } else {
+          reject(`FFmpeg exited with code ${code}`);
+        }
+      });
+    });
+
+    const sticker = await readFile(output);
+    return await embedMetadata(sticker, metadata);
+  } finally {
+    await unlink(output).catch(console.error);
+  }
+}
+
+async function streamToBuffer(stream) {
+  const chunks = [];
+  for await (const chunk of stream) chunks.push(chunk);
+  return Buffer.concat(chunks);
 }
 
 module.exports = createSticker;
